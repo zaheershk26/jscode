f97dffd6d1c04655dbf554518feabb57e61d33c7
Zaheer Sheikh <Zaheer.Sheikh@ocean.ibm.com>
2023-01-06 14:59:03 +0530

    Uploading created JavaScript code

---
+++ myScript.js
@@ -0,0 +1,23 @@
+/*This func is using for 
+printing the name 'Jahnavi'*/
+
+function myFunction() {
+  alert("You have clicked the button!!");
+    document.getElementById("demo").innerHTML = "Hello Jahnavi";
+    //document.getElementById("demo").innerHTML = name+" Reading "+subject;
+    
+  }
+  function add(a, b) {
+    var c = a + b;    
+    document.getElementById("demo").innerHTML = c;  
+  }
+  
+  function multi(a, b) {
+    var c = a * b;    
+    document.getElementById("demo").innerHTML = c;  
+  }
+  function myFunction1() {
+    alert("You have clicked the button For testing!!");
+      
+      
+  }
